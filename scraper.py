92f08f3c3ece3b6adf5f4299003cd24ba18c3fd9
nictait <ngtait@gmail.com>
2014-08-02 18:59:33 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -39,7 +39,7 @@ url = "http://www.who.int/csr/don/archive/disease/ebola/en/"
 root = lxml.html.parse(url).getroot()
 print root
 
-div = root.xpath( '//div[@id="content"]' )
+div = root.xpath( '//div[@id="auto_archive"]' )
 print div
 
 rownum = 0
