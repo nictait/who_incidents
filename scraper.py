eaa5559a1e881fcc0069851839af7fe619fe4d5d
nictait <ngtait@gmail.com>
2014-08-03 12:51:17 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -70,7 +70,7 @@ recno = 0
 for url in urllist:
     print url
     #date_ = (url[43:44] + url[40:41] + url[35:38])
-    date_ = url[43:44]
+    date_ = url[42:44]
     print date_
     root = lxml.html.parse(url).getroot()
     docstr = lxml.html.tostring(root)
